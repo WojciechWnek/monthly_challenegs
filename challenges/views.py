87ece535cd30f872397204b70aec3b6bd4182326
Wojciech Wnęk <wojciech.wnek@profil-software.com>
2023-07-04 16:06:22 +0200

    Add routes

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
+
+# def january(request):
+#   return HttpResponse("Eat no meat")
+
+# def february(request):
+#   return HttpResponse("Walk for 20 min daily")
+
+def monthly_challenge(request, month):
+  month_challenge = None
+  if month == "january":
+    month_challenge="Eat no meat"
+  elif month == "february":
+    month_challenge="Walk 20 min"
+  elif month == "march":
+    month_challenge="Practice Django"
+  else:
+    return HttpResponseNotFound("This month is not supported")
+  return HttpResponse(month_challenge)
