bc595fcb9f9b6cd4b7febb9d435843e0ef400e68
Wojciech Wnęk <wojciech.wnek@profil-software.com>
2023-07-04 16:58:30 +0200

    Dunamic routes

--- challenges/views.py
+++ challenges/views.py
@@ -8,15 +8,27 @@ from django.shortcuts import render
 
 # def february(request):
 #   return HttpResponse("Walk for 20 min daily")
+monthly_challenges = {
+  "january": "Eat no meat",
+  "february": "Walk 20 min",
+  "march": "Practice Django",
+  "april": "Eat no meat",
+  "may": "Walk 20 min",
+  "june": "Practice Django",
+  "july": "Eat no meat",
+  "august": "Walk 20 min",
+  "september": "Practice Django",
+  "october": "Eat no meat",
+  "november": "Walk 20 min",
+  "december": "Practice Django",
+}
+
+def monthly_challenge_by_number(request, month):
+  return HttpResponse(month)
 
 def monthly_challenge(request, month):
-  month_challenge = None
-  if month == "january":
-    month_challenge="Eat no meat"
-  elif month == "february":
-    month_challenge="Walk 20 min"
-  elif month == "march":
-    month_challenge="Practice Django"
-  else:
+  try:
+    month_challenge = monthly_challenges[month]
+    return HttpResponse(month_challenge)
+  except:
     return HttpResponseNotFound("This month is not supported")
-  return HttpResponse(month_challenge)
