37cae1b1e6d2c18fe5566bb8750fb93cabf1fac8
Wojciech Wnek <wojciech.wnek97@gmail.com>
2023-07-06 08:42:46 +0200

    Add tags to html template

--- challenges/views.py
+++ challenges/views.py
@@ -25,16 +25,20 @@ monthly_challenges = {
 }
 
 def index(request):
-  list_items =""
+  # list_items =""
   months = list(monthly_challenges.keys())
 
-  for month in months:
-    capitalized_month = month.capitalize()
-    month_path= reverse("month-challenge", args=[month])
-    list_items +=f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-  response_data = f"<ul>{list_items}</ul>"
+  return render(request, "challenges/index.html", {
+    "months_keys": months
+  })
 
-  return HttpResponse(response_data)
+  # for month in months:
+  #   capitalized_month = month.capitalize()
+  #   month_path= reverse("month-challenge", args=[month])
+  #   list_items +=f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+  # response_data = f"<ul>{list_items}</ul>"
+
+  # return HttpResponse(response_data)
 
 def monthly_challenge_by_number(request, month):
   months = list(monthly_challenges.keys())
@@ -50,7 +54,7 @@ def monthly_challenge(request, month):
     challenge_text = monthly_challenges[month]
     return render(request,"challenges/challenge.html", {
       "text": challenge_text,
-      "month": month.capitalize()
+      "month": month
     })
   except:
     return HttpResponseNotFound("<h1>This month is not supported</h1>")
